40048503afc24aca607fd28dd71c29c55e96ffa9
jordan clayton <jordan_clayton@protonmail.com>
2023-12-15 14:55:54 -0500

    open ai updates

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -1,17 +1,6 @@
 import streamlit as st
 import openai
 import os
-from tqdm import tqdm
-
-# TODO: take this prompt and add it to the chain. It should be the result from this final prompt that is displayed in the text box "take the following article {final_result} and expand upon some of the main and more interesting points. give some deep detail so the reader really gets a meaningful insight into the topics and not just a high level glance. The reader should be able to walk away with actionable knowledge."
-# TODO: add user auth and when user is logged in it adds a button at the bottom that links to the user's Medium account and allows them to post the tweet.
-# TODO: move twitter and linkedin to homepage so don't have to go back and forth
-# TODO: find a way to keep the output generated so it does not have to regenerate every time you go back to the homepage - complete
-# TODO: A+ find a way to allow for the cache to be refreshed when the user changes the input  
-# TODO: maybe make it so user must enter their own api key
-# TODO: B make it so you can save your stories to a database and then you can go back and edit them and also save the prompts to a drop down
-# TODO: A+ add meeting minutes page
-# TODO: add a page that is titled "Need inspiration?" you enter a topic and it gives you a list of blog post titles that you can click on and it will take you to the blog post and then you can copy and paste the title and then you can use that as the title for your blog post
 
 # Hide Streamlit Menu and Footer
 hide_st_style = """
@@ -40,17 +29,16 @@ Please use markdown to format the output from Prompt #2 with a title, headers, a
 @st.cache_data(show_spinner=False)
 def generate_action(user_input):
     with st.spinner("Generating response..."):
-        st.text("") 
+        st.text("")
 
-        base_completion = openai.ChatCompletion.create(
+        # Updated API call
+        base_completion = openai.Completion.create(
             model="gpt-4",
-            messages=[
-                {"role": "system", "content": "You are an AI assistant asked to write a blog post"},
-                {"role": "user", "content": f"{base_prompt_prefix}{user_input}"}
-            ]
+            prompt=f"You are an AI assistant asked to write a blog post. {base_prompt_prefix}{user_input}",
+            max_tokens=150  # Adjust as needed
         )
 
-        base_prompt_output = base_completion.choices[0].message.content.strip()
+        base_prompt_output = base_completion.choices[0].text.strip()
 
         # Build Prompt #2 using GPT-4
         second_prompt = f"""
@@ -65,49 +53,47 @@ def generate_action(user_input):
         References:
         """
 
-        with tqdm(total=100, desc="Generating response", bar_format="{l_bar}{bar} [time left: {remaining}]", colour='green') as pbar:
-            second_prompt_completion = openai.ChatCompletion.create(
-                model="gpt-4",
-                messages=[
-                    {"role": "system", "content": "You are an AI assistant asked to write a blog post"},  
-                    {"role": "user", "content": second_prompt}
-                ]
-            )
+        # Updated API call
+        second_prompt_completion = openai.Completion.create(
+            model="gpt-4",
+            prompt=second_prompt,
+            max_tokens=500  # Adjust as needed
+        )
 
-            second_prompt_output = second_prompt_completion.choices[0].message.content.strip()
+        second_prompt_output = second_prompt_completion.choices[0].text.strip()
 
-            output_parts = second_prompt_output.split("References:")
+        output_parts = second_prompt_output.split("References:")
 
-            citation_prompt = f"""
-            Generate a list of citations for the blog post below: 
+        citation_prompt = f"""
+        Generate a list of citations for the blog post below: 
 
-            {output_parts[0]}
+        {output_parts[0]}
 
-            Citations:
-            """
-            citation_completion = openai.ChatCompletion.create(
-                model="gpt-4",
-                messages=[
-                    {"role": "system", "content": "You are an AI assistant asked to generate citations for a blog post"},
-                    {"role": "user", "content": citation_prompt} 
-                ]
-            )
+        Citations:
+        """
 
-            citation_output = citation_completion.choices[0].message.content.strip()
+        # Updated API call
+        citation_completion = openai.Completion.create(
+            model="gpt-4",
+            prompt=citation_prompt,
+            max_tokens=100  # Adjust as needed
+        )
 
-            final_output = f"{output_parts[0]}\n\nReferences:\n{citation_output}"
+        citation_output = citation_completion.choices[0].text.strip()
 
-            return {"output": final_output}
+        final_output = f"{output_parts[0]}\n\nReferences:\n{citation_output}"
+
+        return {"output": final_output}
 
 st.title("OpenAI Prompt Chaining: Blog Post Generator")
 st.write("Examples: ")
-st.code('Who invented the internet? ')  
-st.code('What is the best way to learn Python? ')
+st.code('Who invented the internet?')  
+st.code('What is the best way to learn Python?')
 st.code('The Science of Sleep: How Our Dreams Shape Our Lives')
 
 user_input = st.text_input("Enter the title of your blog post:") 
 
-# save user input to session state so we can use it in next page
+# Save user input to session state so we can use it in the next page
 st.session_state.user_input = user_input   
 
 result = generate_action(user_input)
